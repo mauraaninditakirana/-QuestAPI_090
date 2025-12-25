dbb49d7fe6298c971673b850270ae3438b6553d5
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-25 18:36:15 +0700

    membuat DetailViewModel untuk kelola data UI utk melihat dan menghapus data siswa

---
+++ app/src/main/java/com/example/praktikumlocalrestapi/viewmodel/DetailViewModel.kt
@@ -0,0 +1,60 @@
+package com.example.praktikumlocalrestapi.viewmodel
+
+@file:OptIn(InternalSerializationApi::class)
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikumlocalrestapi.modeldata.DataSiswa
+import com.example.praktikumlocalrestapi.repositori.RepositoryDataSiswa
+import com.example.praktikumlocalrestapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+sealed interface StatusUiDetail {
+    data class Success(val dataSiswa: DataSiswa) : StatusUiDetail
+    object Error : StatusUiDetail
+    object Loading : StatusUiDetail
+}
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    var statusUiDetail: StatusUiDetail by mutableStateOf(StatusUiDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUiDetail = StatusUiDetail.Loading
+            statusUiDetail = try {
+                StatusUiDetail.Success(dataSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
+            } catch (e: IOException) {
+                StatusUiDetail.Error
+            } catch (e: HttpException) {
+                StatusUiDetail.Error
+            }
+        }
+    }
+
+    @SuppressLint("SuspiciousIndentation")
+    suspend fun hapusSatuSiswa() {
+        val resp: Response<Void> = repositoryDataSiswa.hapusSatuSiswa(idSiswa)
+
+        if (resp.isSuccessful){
+            println("Sukses Hapus Data : ${resp.message()}")
+        } else {
+            println("Gagal Hapus Data : ${resp.errorBody()}")
+        }
+    }
+}
